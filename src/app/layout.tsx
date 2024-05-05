6d8c7d1b7bc40a71f4f6c3bd5a692b67873597cc
Noe TATOUD <noe.tatoud@gmail.com>
2024-05-05 15:46:35 +0200

    feat(setup): Add prettier with plugins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          inter.className,
+          "antialised flex min-h-screen flex-grow bg-gray-900 font-sans tracking-tight text-white",
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
